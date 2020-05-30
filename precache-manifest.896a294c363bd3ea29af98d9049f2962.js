fa5651753b710aac64608ad2bfbd5a45e9f27515
onuiteshi <emmanuelonuiteshi@gmail.com>
2020-05-30 14:28:25 +0100

    Updates

---
+++ precache-manifest.896a294c363bd3ea29af98d9049f2962.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41af43758096ee0358108f2f7b3fbaf5",
+    "url": "/my-covid19tracker/index.html"
+  },
+  {
+    "revision": "e64b36f1ca286f65d946",
+    "url": "/my-covid19tracker/static/css/main.6f0e3d42.chunk.css"
+  },
+  {
+    "revision": "19ca3447f518b7364452",
+    "url": "/my-covid19tracker/static/js/2.4da22285.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/my-covid19tracker/static/js/2.4da22285.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e64b36f1ca286f65d946",
+    "url": "/my-covid19tracker/static/js/main.14817877.chunk.js"
+  },
+  {
+    "revision": "e566658b4d3ed2c1e3f0",
+    "url": "/my-covid19tracker/static/js/runtime-main.e8fe293b.js"
+  },
+  {
+    "revision": "5ea40fb38283bbb1c848d78d5f73515a",
+    "url": "/my-covid19tracker/static/media/covid19image.5ea40fb3.jpg"
+  }
+]);
